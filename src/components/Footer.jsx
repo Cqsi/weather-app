ead210b45d2118a305047d82299d8812d2a98092
Casimir Rönnlöf <casimirr04@gmail.com>
2025-05-09 14:31:19 +0300

    GithubButton package added, not sure whether to keep it

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,20 @@
-
+import GitHubButton from 'react-github-btn';
 
 function Footer() {
-    return (
-        <footer>
-            <p>This is the footer</p>
-        </footer>
-    )
+  return (
+    <footer>
+      <p>Casimir Rönnlöf © 2025</p>
+      <GitHubButton
+        href="https://github.com/Cqsi/weather-app"
+        data-icon="octicon-star"
+        data-size="large"
+        data-show-count={true}
+        aria-label="Star Cqsi/weather-app on GitHub"
+      >
+        Star this project
+      </GitHubButton>
+    </footer>
+  );
 }
 
 export default Footer;
